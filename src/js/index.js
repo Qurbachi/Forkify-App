3a632c07fa01c51b3abe51b916b150b3f88e30da
Damian Tworzydło <damian.tworzydlo17@gmail.com>
2020-04-01 17:07:43 +0200

    Sets the basic structure of the app. babel, webpack are installed and configured. App can fetch data from external API. It porcesses data from API. Recives an input, clears the input after every request, clears the results of the request if the query changes.

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,37 @@
-// Global app controller
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
+
+/*
+*Global state of the app
+* - Search object
+* - current recipe object
+* - Shopping list object
+* - liked recipes
+*/
+const state = {};
+
+const controllSearch = async () => {
+    //1) Get query from view
+    const query = searchView.getInput();
+
+    if(query) {
+        //2) New Search object and add to view
+        state.search = new Search(query);
+
+        //3) Prepare UI for displaying result (proper animation)
+        searchView.clearInput();
+        searchView.clearResults();
+        //4) Search for the recipe
+        await state.search.getResults();
+
+        //5) Display the result
+        searchView.renderResults(state.search.result);
+    }
+};
+
+elements.searchForm.addEventListener('submit', event => {
+    event.preventDefault();
+    controllSearch();
+    console.log(state);
+});
